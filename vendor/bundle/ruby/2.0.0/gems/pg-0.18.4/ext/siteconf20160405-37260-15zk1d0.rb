1105af3719c5c1a9d7e0fc902029ef2dc8cc2f5c
liv123 <olivia.e@gmail.com>
2016-04-06 15:19:47 -0700

    ruby and pg version updatewith rvm

--- vendor/bundle/ruby/2.0.0/gems/pg-0.18.4/ext/siteconf20160405-37260-15zk1d0.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20160405-37260-1xi6cv5"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
